447624ef0198dc876a190c880fed6de290a1cda4
Rudashi <rudashi@gmail.com>
2022-09-15 16:18:58 +0200

    chore: Change Line separator

---
+++ dist/types/markdown.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.defaultConfiguration = void 0;
+exports.defaultConfiguration = {
+    html_input: 'ALLOW',
+    headerIds: false,
+};
+//# sourceMappingURL=markdown.js.map
